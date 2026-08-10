146e28ede1c4b4ca8d54fa8fe18a3067687891b2
Aditya Rathod <adityarathod8085@gmail.com>
2026-08-11 01:16:35 +0530

    Update tunnel URL for sharing

--- js/songs-data.js
+++ js/songs-data.js
@@ -4,7 +4,7 @@ const playlists = [];
 
 const BACKEND_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.protocol === 'file:') 
     ? 'http://127.0.0.1:5000' 
-    : '';
+    : 'https://treated-sheffield-seconds-leslie.trycloudflare.com';
 
 async function loadTrendingSongs() {
     try {
